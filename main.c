5d2eeb05b051bf90c59b3975b96c0bd4443a45c2
fcalve4 <freddycalve@gmail.com>
2026-04-30 15:08:33 +0000

    made some changes

--- main.c
+++ main.c
@@ -1,59 +1,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
 #define MEMORY_SIZE 4096
 
-void load_rom(char* f)
+void load_rom(char* f, uint16_t* memory)
 {
-	FILE* file = fopen(f, "rb");
+	FILE* file_ptr = fopen(f, "rb");
 	
-	if(file == NULL) {
+	if(file_ptr == NULL) {
 		fprintf(stderr,"Unable to open open file \n");
 	}	
 	
     // Get the size of the file
-	fseek(file, 0, SEEK_END);
-	int file_size = ftell(file); 
-	fseek(file, 0, SEEK_SET);
+	fseek(file_ptr, 0, SEEK_END);
+	int file_size = ftell(file_ptr); 
+	fseek(file_ptr, 0, SEEK_SET);
 
-	fread(memory+0x200, sizeof(uint16_t), file_size, file);
-}
-
-void execute() {
-    // Grab one instruction
-    // Decode
-    // Execute
+	fread(memory+0x200, sizeof(uint16_t), file_size, file_ptr);
 }
 
 
-int main(int arc, char** argv) {
+int main(int argc, char** argv) {
 
-    uint16_t PC;
-    uint16_t sp;
-    uint16_t I; 
+    if (argc < 2) {
+		printf("Usage: ./a.out <rom> \n");
+		return 1;
+	}
+
+    
+
+    //uint16_t PC;
+    //uint16_t sp;
+    //uint16_t I; 
 
     uint16_t memory[MEMORY_SIZE];
-    uint16_t stack[16];
-    uint8_t v[16];
+    //uint16_t stack[16];
+    //uint8_t v[16];
 
-    uint8_t delay_timer;
-    uint8_t sound_timer;
+    //uint8_t delay_timer;
+    //uint8_t sound_timer;
 
-    memset(stack,0,16);
-	memset(memory,0,4096);
-	memset(v,0,16);
+    load_rom(argv[1], memory);
 
 
-    // Fetch
-    // Decode
-    // Execute
-    
-
-    while(1) {
-        execute();
-    }
-
-    
     return 0;
 }
